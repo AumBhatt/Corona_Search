af92d25d28a4510a3f8391f5cbf7d5164dd94c4e
Aum Ashokbhai Bhatt <aum9698@outlook.com>
2020-08-07 00:52:06 +0400

    Offline Trial...

--- js/sw.js
+++ js/sw.js
@@ -1,7 +1,20 @@
 console.log('sw.js');
-self.addEventListener('install', function(ev){
-    console.log('SW Installed!!');
+var cache_name = "cov19Track"; // The string used to identify our cache
+
+self.addEventListener("install", event => {
+    console.log("installing...");
+    event.waitUntil(
+        caches
+            .open(cache_name)
+            .then(cache => {
+                return cache.addAll(assets);
+            })
+            .catch(err => console.log(err))
+    );
 });
+
+
+
 
 self.addEventListener('activate', function(ev){
     console.log('SW Activated!');
@@ -9,4 +22,18 @@ self.addEventListener('activate', function(ev){
 
 self.addEventListener('fetch', function(ev){
     console.log('Fetch ev');
+    if (event.request.url === "https://aumbhatt.github.io/ProjectX") {
+        // or whatever your app's URL is
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                self.cache.open(cache_name).then(cache => cache.match("/offline.html"))
+            )
+        );
+    } else {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                caches.match(event.request).then(response => response)
+            )
+        );
+    }
 });

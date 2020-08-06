55eaf00177edb27aa3dce9212aa609ce58c33b42
Aum Ashokbhai Bhatt <aum9698@outlook.com>
2020-08-07 01:13:49 +0400

    Installability 1...

--- js/install.js
+++ js/install.js
@@ -3,6 +3,7 @@ var deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
+  console.log(e);
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;

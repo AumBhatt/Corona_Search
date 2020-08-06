13e141b9fbacb603c22884992df9da78815c0203
Aum Ashokbhai Bhatt <aum9698@outlook.com>
2020-08-07 01:29:53 +0400

    Install 4..........

--- js/install.js
+++ js/install.js
@@ -4,9 +4,9 @@ var deferredPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   console.log(e);
-  e.preventDefault();
+  /* e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can install the PWA
-  showInstallPromotion();
+  showInstallPromotion(); */
 });

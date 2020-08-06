70d39c1dd592695cdfc9b24e5188bdf9b99f6f3f
Aum Ashokbhai Bhatt <aum9698@outlook.com>
2020-08-07 02:24:45 +0400

    Major Update 2

--- js/install.js
+++ js/install.js
@@ -8,18 +8,47 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can install the PWA
+  if(localStorage.getItem('appInstalled') === 'true')
   showInstallPrompt();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  let displayMode = 'browser tab';
+  if (navigator.standalone) {
+    displayMode = 'standalone-ios';
+  }
+  if (window.matchMedia('(display-mode: standalone)').matches) {
+    displayMode = 'standalone';
+  }
+  // Log launch display mode to analytics
+  hidePrompt();
+  console.log('DISPLAY_MODE_LAUNCH:', displayMode);
+});
+
+
+window.addEventListener('appinstalled', (evt) => {
+  // Log install to analytics
+  localStorage.setItem('appInstalled', 'true');
+  console.log('INSTALL: Success');
+  hidePrompt();
 });
 
 function showInstallPrompt(){
   document.querySelector('.a2hs').getElementsByClassName.animationName = "a2hs";
 }
 
+function hidePrompt(){
+  installBtnShow.style.display = "none";
+  document.querySelector('.instPrompt').style.display = "none";
+
+}
+
 var installBtnShow = document.querySelector('.a2hs-btn');
 
 installBtnShow.addEventListener('click', function(e){
   deferredPrompt.prompt();
-
+  installBtnShow.style.display = "none";
+  document.querySelector('.instPrompt').style.display = "flex";
   deferredPrompt.userChoice.then(function(choiceResult){
     if(choiceResult.outcome === 'accepted'){
       console.log('Installation Accepted by User!');

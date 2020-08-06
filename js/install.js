ccf7497b0ce22d10beeb25b7e2917eb6db603897
Aum Ashokbhai Bhatt <aum9698@outlook.com>
2020-08-07 01:47:43 +0400

    Install Maybe.....

--- js/install.js
+++ js/install.js
@@ -4,9 +4,28 @@ var deferredPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   console.log(e);
-  /* e.preventDefault();
+  e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can install the PWA
-  showInstallPromotion(); */
+  showInstallPrompt();
+});
+
+function showInstallPrompt(){
+  document.querySelector('.a2hs').getElementsByClassName.animationName = "a2hs";
+}
+
+var installBtnShow = document.querySelector('.a2hs-btn');
+
+installBtnShow.addEventListener('click', function(e){
+  deferredPrompt.prompt();
+
+  deferredPrompt.userChoice.then(function(choiceResult){
+    if(choiceResult.outcome === 'accepted'){
+      console.log('Installation Accepted by User!');
+    }
+    else{
+      console.log('Installation Dismissed!');
+    }
+  });
 });
